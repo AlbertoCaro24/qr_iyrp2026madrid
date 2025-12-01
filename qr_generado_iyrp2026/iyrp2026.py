d005d5f28c53fa364a0812bf8cad7a1d998f44c8
AlbertoCaro24 <alber24caro@gmail.com>
2025-12-01 15:56:25 +0100

    V1.03

--- qr_generado_iyrp2026/iyrp2026.py
+++ qr_generado_iyrp2026/iyrp2026.py
@@ -2,14 +2,14 @@ import qrcode
 import os
 
 # Crear carpeta si no existe
-os.makedirs("qr_generado_grafton_exfingidor", exist_ok=True)
+os.makedirs("qr_generado_iyrp_2026_madrid", exist_ok=True)
 
-base_url = "https://es.grafton.com/es/exfingidor"
+base_url = "https://albertocaro24.github.io/qr_iyrp2026madrid/"
 
 # Generar el código QR
 img = qrcode.make(base_url)
 
 # Guardar la imagen dentro de la carpeta
-img.save("qr_generado_grafton_exfingidor/qr_grafton_exfingidor.png")
+img.save("qr_generado_iyrp_2026_madrid/qr_iyrp_2026_madrid.png")
 
 print("QR generado correctamente.")
